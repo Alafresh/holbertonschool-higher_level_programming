e06dd0fd03c62d49097246240a3db6d0681d3a52
Alafresh <868@holbertonschool.com>
2019-09-10 23:12:47 +0000

    Smile in the mirror!

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-odd = ""
 for i in range(122, 96, -1):
     if i % 2 == 1:
-        odd = chr(i - 32)
-    else:
-        odd = chr(i)
-    print(odd, end="")
+        i = i - 32
+    print("{}".format(chr(i)), end="")
