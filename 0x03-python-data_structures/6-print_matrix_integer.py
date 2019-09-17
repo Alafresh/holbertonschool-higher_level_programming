cb1ca24256cfc929f80c9ec54e2e60ac7aba84e6
Alafresh <868@holbertonschool.com>
2019-09-17 20:35:44 +0000

    list of lists = Matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,11 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return None
     for row in matrix:
+        count = 0
         for elem in row:
-            print("{}".format(elem), end=' ')
+            if len(row) - 1 > count:
+                print("{}".format(elem), end=' ')
+            else:
+                print("{}".format(elem), end='')
+            count += 1
         print()
