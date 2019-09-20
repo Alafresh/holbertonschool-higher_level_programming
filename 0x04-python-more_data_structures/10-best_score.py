29143544d5ee279b76cb33da617f3260a9909b65
Alafresh <868@holbertonschool.com>
2019-09-20 00:03:07 +0000

    Best score!

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return None if not a_dictionary else max(a_dictionary)
+    bigger = 0
+    bigger_str = ""
+    if not a_dictionary:
+        return None
+    for i in a_dictionary:
+        if a_dictionary[i] > bigger:
+            bigger = a_dictionary[i]
+            bigger_str = i
+    return bigger_str
