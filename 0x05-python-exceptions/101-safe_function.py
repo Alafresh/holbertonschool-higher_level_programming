7495157e4b0877f04d36f7a0e423c1b329bd9a5d
Alafresh <868@holbertonschool.com>
2019-09-30 23:41:16 +0000

    safe function!

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,12 +3,13 @@ from sys import stderr
 
 
 def safe_function(fct, *args):
+    a = 0
     try:
-        fct(*args)
+        a = fct(*args)
     except ZeroDivisionError:
         print("Exception: division by zero", file=stderr)
         return None
     except IndexError:
         print("Exception: list index out of range", file=stderr)
         return None
-    return fct(*args)
+    return a
