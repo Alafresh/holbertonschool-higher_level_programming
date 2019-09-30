d620ee5226ae1bad844144f675754d3876db9151
Alafresh <868@holbertonschool.com>
2019-09-30 21:00:36 +0000

    Divide a listgit add 4-list_division.py !

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,4 +13,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except ZeroDivisionError:
             print("division by 0")
             new_list[i] += 0
+        finally:
+            pass
     return new_list
