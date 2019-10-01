d89b5041bb3cd7b30abc498d2576e65f7e0653dd
Alafresh <868@holbertonschool.com>
2019-10-01 21:42:29 +0000

    printing a square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size=0):
-        if not isinstance(size,(int)):
+        if not isinstance(size, (int)):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
@@ -28,5 +28,5 @@ class Square:
             print()
         for i in range(self.__size):
             for j in range(self.__size):
-                print('{}'.format('#'), end='')
+                print("#", end=" ")
             print()
