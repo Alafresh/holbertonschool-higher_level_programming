234f9ca0a8f71599a2c7c7b0fc14fb38aea59aab
Alafresh <868@holbertonschool.com>
2019-10-01 02:49:30 +0000

    add one case error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except (NameError, ValueError) as err:
         print("Exception: {}".format(err), file=stderr)
         return False
