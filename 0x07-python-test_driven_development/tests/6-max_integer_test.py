0114f55b4ed1b35a46b9dea6df7c26c9a1710898
Alafresh <868@holbertonschool.com>
2019-10-08 13:34:56 +0000

    change the name functions

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,17 +10,17 @@ class TestMaxInteger(unittest.TestCase):
     def test_max(self):
         self.assertAlmostEqual(max_integer([50, 40, 60]), 60)
 
-    def max_at_the_beginning(self):
+    def test_max_at_the_beginning(self):
         self.assertAlmostEqual(max_integer([110, 40, 60, 30]), 110)
 
-    def max_in_the_middle(self):
+    def test_max_in_the_middle(self):
         self.assertAlmostEqual(max_integer([50, 40, 90, 30, 15]), 90)
 
-    def one_negative(self):
+    def test_one_negative(self):
         self.assertAlmostEqual(max_integer([-50, -40, -60, -30]), -60)
 
-    def list_of_one(self):
+    def test_list_of_one(self):
         self.assertAlmostEqual(max_integer([60]), 60)
 
-    def empty_list(self):
+    def test_empty_list(self):
         self.assertAlmostEqual(max_integer([]), None)
