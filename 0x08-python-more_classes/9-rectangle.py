ae34b06c4d4eef50ce37ad894dafff81bfb5b951
Alafresh <868@holbertonschool.com>
2019-10-08 15:09:35 +0000

    del slef

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -17,7 +17,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        elif self.__width < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
@@ -29,7 +29,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        elif self.__height < 0:
+        elif value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
 
