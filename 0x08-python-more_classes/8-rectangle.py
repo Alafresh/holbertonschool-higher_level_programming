ba7ba87ea6ebea365cf7012fbbf6099969737b3c
Alafresh <868@holbertonschool.com>
2019-10-08 14:59:26 +0000

    value

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         type(self).number_of_instances += 1
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -17,7 +17,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        elif self.__width < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
         self.__width = value
 
@@ -29,7 +29,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        elif self.__height < 0:
+        elif value < 0:
             raise ValueError('height must be >= 0')
         self.__height = value
 
