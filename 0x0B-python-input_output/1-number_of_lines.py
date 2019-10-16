4022f07ac0b0d543d2058f4102eca5a0bbd9539e
Alafresh <868@holbertonschool.com>
2019-10-16 23:11:57 +0000

    Number of lines

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pyhton3
 def number_of_lines(filename=""):
-    with open("my_file_0.txt") as myFile:
+    with open(filename) as myFile:
         count = 0
         for line in myFile:
             count += 1
