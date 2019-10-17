960657e37d0f7c35cd7710338dea35a2f111e5f6
Alafresh <868@holbertonschool.com>
2019-10-17 04:06:11 +0000

    I dont know

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,5 +1,5 @@
 #!/usr/bin/pyhton3
 def number_of_lines(filename=""):
-    with open(filename) as myFile:
+    with open(filename, encoding="utf-8") as myFile:
         number = myFile.readlines()
     return len(number)
