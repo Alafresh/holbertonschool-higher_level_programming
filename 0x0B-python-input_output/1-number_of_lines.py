2e02a9ac00840983936e27eda4d371c9386ef1a7
Alafresh <868@holbertonschool.com>
2019-10-17 04:03:31 +0000

    using methods to get the number of lines

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,7 +1,5 @@
 #!/usr/bin/pyhton3
 def number_of_lines(filename=""):
     with open(filename) as myFile:
-        count = 0
-        for line in myFile:
-            count += 1
-    return count
+        number = myFile.readlines()
+    return len(number)
