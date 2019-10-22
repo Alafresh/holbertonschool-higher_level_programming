3ea92c7944bd8f0e70835cbcf52ef0400ee51f45
Alafresh <868@holbertonschool.com>
2019-10-22 19:06:13 +0000

    add docstring

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -7,6 +7,7 @@ class Rectangle(Base):
     """Base class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """method to build rectangle"""
         self.width = width
         self.height = height
         self.x = x
@@ -15,32 +16,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """getter of the width"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter of the width"""
         self.__width = value
 
     @property
     def height(self):
+        """getter of the height"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter of the height"""
         self.__height = value
 
     @property
     def x(self):
+        """getter of the x"""
         return self.__x
 
     @x.setter
     def x(self, value):
+        """setter of the x"""
         self.__x = value
 
     @property
     def y(self):
+        """getter of the y"""
         return self.__y
 
     @y.setter
     def y(self, value):
+        """setter of the y"""
         self.__y = value
