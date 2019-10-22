e39ad40d0faffa0b7c8e204aa1badc0cf4756599
Alafresh <868@holbertonschool.com>
2019-10-22 22:29:06 +0000

    overloading __str__ method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,3 +9,10 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """constructor initialize"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        name = self.__class__.__name__
+        return "[{}] ({}) {}/{} - {}".format(
+                name, self.id,
+                self.x, self.y,
+                self.width)
