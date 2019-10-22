1a3a0653ccb6d4376b788d4b2c641ae9bead781e
Alafresh <868@holbertonschool.com>
2019-10-22 23:45:02 +0000

    JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,7 +16,20 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Dictionary to Json string"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return dumps([])
         else:
             return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """JSON string to file"""
+        list_o = []
+        name = cls.__name__
+        nameC = "{}{}".format(name, ".json")
+        if list_objs:
+            for objects in list_objs:
+                list_o.append(objects.to_dictionary())
+        with open(nameC, 'w', encoding='utf-8') as json_file:
+            json_file.write(cls.to_json_string(list_o))
