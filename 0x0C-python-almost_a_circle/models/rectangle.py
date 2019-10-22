c67589a2475b3f3a9caaae3e8278696ec24ad98f
Alafresh <868@holbertonschool.com>
2019-10-22 19:58:40 +0000

    prints stdout th rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,10 @@ class Rectangle(Base):
     def area(self):
         """method that calculate the area"""
         return self.__width * self.__height
+
+    def display(self):
+        """method to print rectangle"""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                print('#', end="")
+            print()
