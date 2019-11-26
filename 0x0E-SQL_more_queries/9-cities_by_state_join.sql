d7fad7b45d569b5bd8f619ca7180ddc8e5a94fda
Alafresh <868@holbertonschool.com>
2019-11-26 14:31:44 -0500

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- States tables
-SELECT id.*, name.*, name.* FROM cities id name, states name WHERE state_id = id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id;
