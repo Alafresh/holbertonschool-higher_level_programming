2f76ca1b36393cd5e041fe4121ddb9076733a714
Alafresh <868@holbertonschool.com>
2019-11-26 16:14:08 -0500

    SELECT tv_genres.name

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- Number of shows by genre
+SELECT tv_genres.name AS genre, COUNT(genre_id) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id GROUP BY genre HAVING number_of_shows > 0 ORDER BY number_of_shows DESC;
