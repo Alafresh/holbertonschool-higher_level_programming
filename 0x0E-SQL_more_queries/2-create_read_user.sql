c0d5619e940227ddc0fd69fc41697434bd5b18fe
Alafresh <868@holbertonschool.com>
2019-11-26 12:56:23 -0500

    read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- READ USER
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER user_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON hbtn_0d_2 TO user_0d_2@localhost;
+CREATE USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
