f91f138db3e42388dea21f7f8e5ab922af243e7e
Alafresh <868@holbertonschool.com>
2019-11-26 12:28:57 -0500

    root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,2 @@
 -- Root user
-CREATE USER user_0d_1@localhost IDENTIFIED BY user_0d_1;
-GRANT ALL TO user_0d_1@localhost;
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
