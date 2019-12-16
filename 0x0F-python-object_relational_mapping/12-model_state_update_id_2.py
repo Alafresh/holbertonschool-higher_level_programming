b7e92a2030b9e6f8729f99289e2082e10700ff9c
Alafresh <868@holbertonschool.com>
2019-12-15 21:38:49 -0500

    Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" List all state objects from the database """
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    ses = Session(engine)
+    state_n = ses.query(State).filter_by(id='2').first()
+    state_n.name = 'New Mexico'
+    ses.commit()
+    ses.close()
