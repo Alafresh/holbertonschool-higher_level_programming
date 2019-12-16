eca30801b7074b224c95cf1c34308d2415705850
Alafresh <868@holbertonschool.com>
2019-12-15 20:22:42 -0500

    Contains a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" List all state objects from the database """
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    ses = Session(engine)
+    state_a = ses.query(State).filter(State.name.like('%a%')).all()
+    for i in state_a:
+        print("{}: {}".format(i.id, i.name))
+    ses.close()
