4b394a365bade6cef572e6f49de88a094a088b5e
Alafresh <868@holbertonschool.com>
2020-01-06 19:29:46 +0000

    CUrl to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response
-curl -X GET -o /dev/null --silent --head --write-out '%{http_code}\n' "$1"
+curl -Lsf "$1"
+#curl -s -I "$1" | grep HTTP/ | awk {"print $2"}
+#curl -X GET -o /dev/null --silent --head --write-out '%{http_code}\n' "$1"
