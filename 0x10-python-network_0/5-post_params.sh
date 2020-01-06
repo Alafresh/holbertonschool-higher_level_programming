0a5fedd844a6d57b98d044095a5069922fe32d5b
Alafresh <868@holbertonschool.com>
2020-01-06 21:26:41 +0000

    post parameters!

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response
-curl -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" -H "X-Holber
+curl -Lsd "email=hr@holbertonschool.com&subject=I will always be here for PLD" -H "X-Holber
 tonSchool-User-Id: 98" -X 'POST' "$1"
