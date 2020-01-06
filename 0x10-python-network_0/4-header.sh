518ce3162bea2694e2c14417fc21de2fe9ca5edf
Alafresh <868@holbertonschool.com>
2020-01-06 20:53:14 +0000

    Curl headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to that URL, and displays the size of the body of the response
-curl -Ls "$1" -X 'GET' -H "X-HolbertonSchool-User-Id :98"
+curl -Ls "$1" -X 'GET' -H "X-HolbertonSchool-User-Id: 98"
