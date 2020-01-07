c120856bafd5f377b6324b4fae69d9dec1303fea
Alafresh <868@holbertonschool.com>
2020-01-07 15:56:54 +0000

    find the peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# This script finds a peak in a list of integers
+def find_peak(list_of_integers):
+    if len(list_of_integers) == 0:
+        return None
+    n = len(list_of_integers)
+    low = 0
+    high = len(list_of_integers) - 1
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+    return low
