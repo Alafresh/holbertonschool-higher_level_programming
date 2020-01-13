ff84aa232c20814e9397714f8433836a0e6af925
Alafresh <868@holbertonschool.com>
2020-01-13 17:43:52 +0000

    add t

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         data = html
-    print('Body response:')
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(data.decode('UTF-8')))
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(data.decode('UTF-8')))
