cd31bab78e7c3751b36d9b0727fc369aaf6b235d
Alafresh <868@holbertonschool.com>
2020-01-13 17:33:39 +0000

    add body response

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" this is a comment"""
 if __name__ == "__main__":
     from urllib import request
     with request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         data = html
+    print('Body response:')
     print('\t- type: {}'.format(type(html)))
     print('\t- content: {}'.format(html))
     print('\t- utf8 content: {}'.format(data.decode('UTF-8')))
