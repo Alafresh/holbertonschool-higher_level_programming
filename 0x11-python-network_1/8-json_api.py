2aed33b9c0281f1665a9deef8aef5937fbec053e
Alafresh <868@holbertonschool.com>
2020-01-13 22:16:10 -0500

    Search Api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from requests import get, head, options, post
+    from requests import get, head, post
     from sys import argv
     try:
-        r = post('http://0.0.0.0:5000/search_user', data={'q': argv[1]})
+        if len(argv) == 2:
+            data = {'q': argv[1]}
+        else:
+            data = {'q': ''}
+        r = post('http://0.0.0.0:5000/search_user', data)
         res = r.json()
         print('[{}] {}'.format(res['id'], res['name']))
     except KeyError:
