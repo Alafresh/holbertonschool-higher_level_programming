88588a10f92945676f112b6fd4c324a1c0fc95ba
Alafresh <868@holbertonschool.com>
2020-02-04 12:03:21 -0500

    fix typo missing letter a

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@ const command = process.argv;
 const number = parseInt(command[2]);
 
 if (isNaN(number)) {
-  console.log('Not number');
+  console.log('Not a number');
 } else {
   console.log('My number: ' + number);
 }
