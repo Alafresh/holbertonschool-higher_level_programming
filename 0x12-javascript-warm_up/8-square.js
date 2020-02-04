cc48d138ef659b6137e80c2130b0906fbabc0d34
Alafresh <868@holbertonschool.com>
2020-02-04 12:33:31 -0500

    Check if first argument cant be converted to an integer

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
-/*
-    Print a square with the character #
-
-    The size of the square must be the first argument
-    of the program.
-*/
 const square = 'X';
+const size = process.argv[2];
 
 if (process.argv.length <= 2) {
   console.log('Missing size');
-}
-
-if (typeof process.argv[2] === 'string') {
+} else if (isNaN(size)) {
   console.log('Missing size');
 }
-const size = process.argv[2];
 
 for (let i = 0; i < size; i++) {
   console.log(square.repeat(size));
