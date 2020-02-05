7b35819279167e7eec2c5796e98aed0a489fb7ed
Alafresh <868@holbertonschool.com>
2020-02-04 20:12:22 -0500

    Second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const arg = process.argv;
+const array = [];
+const array2 = [];
+
+if (arg.length <= 3) {
+  console.log(0);
+}
+
+for (let i = 2; i < arg.length; i++) {
+  array.push(Number(arg[i]));
+}
+for (let j = 0; j < array.length; j++) {
+  if (array[j] !== Math.max(...array)) {
+    array2.push(array[j]);
+  }
+}
+console.log(Math.max(...array2));
