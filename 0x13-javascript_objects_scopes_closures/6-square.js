cb7d115210be3701f17bcc7435224af7c4c2acb9
Alafresh <868@holbertonschool.com>
2020-02-10 15:00:33 -0500

    Fix ethe inherits

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const Rectangle = require('./5-square.js');
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
 
   charPrint (c) {
-    if (typeof c == 'undefined') {
+    if (typeof c === 'undefined') {
       c = 'X';
     }
     for (let i = 0; i < this.height; i++) {
