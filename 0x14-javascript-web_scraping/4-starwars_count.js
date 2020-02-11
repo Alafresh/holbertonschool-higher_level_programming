4e4c6278561dfbe61a80d299af61fd752760e71a
Alafresh <868@holbertonschool.com>
2020-02-11 13:03:48 -0500

    I dont know what is wrong

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,10 +3,10 @@ const url = process.argv[2];
 const request = require('request');
 
 request(url, function (err, response, body) {
-  let count = 0;
   if (err) {
     console.log(err);
   } else {
+    let count = 0;
     const results = JSON.parse(body).results;
     for (let i = 0; i < results.length; i++) {
       for (let j = 0; j < results[i].characters.length; j++) {
