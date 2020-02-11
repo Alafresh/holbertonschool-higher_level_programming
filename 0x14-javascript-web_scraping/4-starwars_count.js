5822ba09657b078ff5cb1ad9f2c0ff7dbd8b5e28
Alafresh <868@holbertonschool.com>
2020-02-11 12:54:23 -0500

    Star wars Wedge Antilles

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const url = process.argv[2];
+const request = require('request');
+
+request(url, function (err, response, body) {
+  let count = 0;
+  if (err) {
+    console.log(err);
+  } else {
+    const results = JSON.parse(body).results;
+    for (let i = 0; i < results.length; i++) {
+      for (let j = 0; j < results[i].characters.length; j++) {
+        if (results[i].characters[j] === 'https://swapi.co/api/people/18/') {
+          count += 1;
+        }
+      }
+    }
+    console.log(count);
+  }
+});
